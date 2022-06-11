c099ed0b40c141338a8cca582061025e213994c5
Arnav Shah <arnavshah4444@gmail.com>
2022-06-11 21:30:04 +0000

    Remove unnecesary file, add form for contacting

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -12,14 +12,20 @@ public class FormHandlerServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    // Get the values entered in the form.
+    String name = request.getParameter("name");
+    String email = request.getParameter("email");
+    String message = request.getParameter("message");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("Name: " + name);
+    System.out.println("Email: " + email);
+    System.out.println("Message: " + message);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("Name: " + name);
+    response.getWriter().println("Email: " + email);
+    response.getWriter().println("Message: " + message);
 
     response.sendRedirect("https://ashah-sps-summer22.appspot.com/");
   }
