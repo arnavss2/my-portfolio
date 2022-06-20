70931756814a882a6000318234ad83a782bdd7b5
Arnav Shah <arnavshah4444@gmail.com>
2022-06-20 00:10:08 +0000

    Add variables for constants in servlet

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,34 +13,41 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
+    private static String CONTACT_CONST = "Contact";
+    private static String SMALL_NAME_CONST = "name";
+    private static String SMALL_EMAIL_CONST = "email";
+    private static String SMALL_MESSAGE_CONST = "message";
+    private static String NAME_CONST = "Name: ";
+    private static String EMAIL_CONST = "Email: ";
+    private static String MESSAGE_CONST = "Message: ";
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the values entered in the form.
-    String name = request.getParameter("name");
-    String email = request.getParameter("email");
-    String message = request.getParameter("message");
+    String name = request.getParameter(SMALL_NAME_CONST);
+    String email = request.getParameter(SMALL_EMAIL_CONST);
+    String message = request.getParameter(SMALL_MESSAGE_CONST);
 
     // Print the value so you can see it in the server logs.
-    System.out.println("Name: " + name);
-    System.out.println("Email: " + email);
-    System.out.println("Message: " + message);
+    System.out.println(NAME_CONST + name);
+    System.out.println(EMAIL_CONST + email);
+    System.out.println(MESSAGE_CONST + message);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("Name: " + name);
-    response.getWriter().println("Email: " + email);
-    response.getWriter().println("Message: " + message);
+    response.getWriter().println(NAME_CONST + name);
+    response.getWriter().println(EMAIL_CONST + email);
+    response.getWriter().println(MESSAGE_CONST + message);
     
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Contact");
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind(CONTACT_CONST);
 
     FullEntity contactEntity =
     Entity.newBuilder(keyFactory.newKey())
-        .set("name", name)
-        .set("email", email)
-        .set("message", message)
+        .set(NAME_CONST, name)
+        .set(EMAIL_CONST, email)
+        .set(MESSAGE_CONST, message)
         .build();
 
     datastore.put(contactEntity);
